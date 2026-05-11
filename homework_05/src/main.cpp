5530ee5b6da6dc64dc608f3a1a609112aff968d8
mykytaky <nikitakyrpenko@gmail.com>
2026-05-11 06:33:48 +0000

    handle empty file

--- homework_05/src/main.cpp
+++ homework_05/src/main.cpp
@@ -13,6 +13,11 @@ int main(int argc, char** argv)
   Frame frames[MAX_TELEMETRY_FRAMES];
   const int frame_count = read_frames(argv[1], frames, MAX_TELEMETRY_FRAMES);
 
+  if (frame_count == 0) {
+    std::cerr << argv[1] << " is empty\n";
+    return 1;
+  }
+
   const Summary summary = summarize(frames, frame_count);
   print_summary(summary);
 
